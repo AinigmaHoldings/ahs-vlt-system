b902dc4f0aa406c3f556622587fc0fb23e2bee72
Luis Martinez <luis.martinez@ainigmagroup.com>
2024-07-19 18:36:34 -0500

    more docker compose changes

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -26,29 +26,11 @@ sudo systemctl start docker.service
 echo "Enabling Docker service..."
 sudo systemctl enable docker.service
 
+# Installing docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Add the current user to the 'docker' group to manage Docker as a non-root user
 echo "Adding user to docker group..."
 sudo usermod -aG docker ${USER}
 newgrp docker
-
-# Create and navigate to the 'vlt' directory in the home directory
-echo "Creating and navigating to 'vlt' directory..."
-mkdir -p ~/vlt && cd ~/vlt
-
-# Clone the 'ahs-vlt-system' repository from GitHub
-echo "Cloning the repository..."
-git clone https://github.com/AinigmaHoldings/ahs-vlt-system.git
-
-# Navigate into the cloned repository directory
-echo "Navigating into the repository directory..."
-cd ahs-vlt-system
-
-# Build the Docker image with the tag 'ahsvlt'
-echo "Building the Docker image..."
-docker build -t ahsvlt .
-
-# List all Docker images
-echo "Listing Docker images..."
-docker images
-
-echo "Script execution completed."
