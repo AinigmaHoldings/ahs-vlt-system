84a2132c68cd1e91c371befd74238e2595d8b60b
luis.martinez <luis.martinez@ainigmagroup.com>
2024-07-22 11:13:26 -0500

    merging changes office and home

--- cloudflare/VLT_Cloudflare_Install.sh
+++ cloudflare/VLT_Cloudflare_Install.sh
@@ -33,3 +33,28 @@ vi ~/.ssh/config
 Host vlt-lab-0.ainigmaim.com
 ProxyCommand /usr/local/bin/cloudflared access ssh --hostname %h
 #####################################
+
+
+
+################For Prometheus#######################################
+
+ubuntu@ip-10-0-0-221:~/node_exporter-1.8.2.linux-amd64$ cat /etc/cloudflared/config.yml
+tunnel: 995cafce-4966-4c4d-87aa-a63220432205
+credentials-file: /home/ubuntu/.cloudflared/995cafce-4966-4c4d-87aa-a63220432205.json
+ingress:
+  - hostname: vlt-lab-ec2.ainigmaim.com
+    service: http://localhost:9100
+  - hostname: ssh-vlt-lab-ec2.ainigmaim.com
+    service: ssh://localhost:22
+  - service: http_status:404
+
+
+  ubuntu@ip-10-0-0-221:~/node_exporter-1.8.2.linux-amd64$ cat /etc/cloudflared/config.yml
+tunnel: 995cafce-4966-4c4d-87aa-a63220432205
+credentials-file: /home/ubuntu/.cloudflared/995cafce-4966-4c4d-87aa-a63220432205.json
+ingress:
+  - hostname: vlt-lab-ec2.ainigmaim.com
+    service: http://localhost:9100
+  - hostname: ssh-vlt-lab-ec2.ainigmaim.com
+    service: ssh://localhost:22
+  - service: http_status:404
