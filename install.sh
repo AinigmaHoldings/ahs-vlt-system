974a5a88c5bb1681823b7b9ab0d9c31a6516abc0
probua <alejandroraj@gmail.com>
2024-08-08 16:49:33 -0500

    fix: install.sh

--- install.sh
+++ install.sh
@@ -21,32 +21,32 @@ CONFIGURATION_FILE="/home/${USER}/vlt/config/config.cfg"
 # Update confiuration file
 source src/update-configuration-file.sh
 
-# # Read parameters from configuration file
-# source src/read-configuration-file.sh
+# Read parameters from configuration file
+source src/read-configuration-file.sh
 
-# # Files installation
-# TEMPLATE_PRINTENV=$(cat "src/templates/printenv.sh")
-# create_file "/home/${USER}/vlt/printenv.sh" "$TEMPLATE_PRINTENV"
+# Files installation
+TEMPLATE_PRINTENV=$(cat "src/templates/printenv.sh")
+create_file "/home/${USER}/vlt/printenv.sh" "$TEMPLATE_PRINTENV"
 
-# TEMPLATE_XHOSTDOCKER_DESKTOP=$(cat "src/templates/xhostdocker.desktop")
-# create_file "/home/${USER}/.config/autostart/xhostdocker.desktop" "$TEMPLATE_XHOSTDOCKER_DESKTOP"
+TEMPLATE_XHOSTDOCKER_DESKTOP=$(cat "src/templates/xhostdocker.desktop")
+create_file "/home/${USER}/.config/autostart/xhostdocker.desktop" "$TEMPLATE_XHOSTDOCKER_DESKTOP"
 
-# source src/template/config-yml.sh
-# create_file "/home/${USER}/vlt/config.yml" "$TEMPLATE_CONFIG_YML"
+source src/template/config-yml.sh
+create_file "/home/${USER}/vlt/config.yml" "$TEMPLATE_CONFIG_YML"
 
-# source src/templates/vlt-service.sh
-# sudo_create_file "/etc/systemd/system/vlt.service" "$TEMPLATE_VLT_SERVICE"
+source src/templates/vlt-service.sh
+sudo_create_file "/etc/systemd/system/vlt.service" "$TEMPLATE_VLT_SERVICE"
 
-# source src/templates/printenvlocal-desktop.sh
-# create_file "/home/${USER}/.config/autostart/printenvlocal.desktop" "$TEMPLATE_PRINTENVLOCAL_DESKTOP"
+source src/templates/printenvlocal-desktop.sh
+create_file "/home/${USER}/.config/autostart/printenvlocal.desktop" "$TEMPLATE_PRINTENVLOCAL_DESKTOP"
 
-# source src/templates/docker-compose.sh
-# create_file "/home/${USER}/vlt/docker-compose.yml" "$TEMPLATE_DOCKER_COMPOSE_YML"
+source src/templates/docker-compose.sh
+create_file "/home/${USER}/vlt/docker-compose.yml" "$TEMPLATE_DOCKER_COMPOSE_YML"
 
-# # Enable services
-# echo "Enabling Services in systemd"
-# sudo systemctl daemon-reload
-# sudo systemctl enable vlt.service
+# Enable services
+echo "Enabling Services in systemd"
+sudo systemctl daemon-reload
+sudo systemctl enable vlt.service
 
-# # Install docker
-# source src/install-docker.sh
+# Install docker
+source src/install-docker.sh
