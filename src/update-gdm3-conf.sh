5d91757795a8a8a27207cc02afb106cbb89fe865
nexlotlotto <81607417+nexlotlotto@users.noreply.github.com>
2024-09-26 11:24:58 -0500

    Update update-gdm3-conf.sh

--- src/update-gdm3-conf.sh
+++ src/update-gdm3-conf.sh
@@ -1,7 +1,7 @@
 config_file="/etc/gdm3/custom.conf"
 
 # Remove all lines that contain 'WaylandEnable'
-sed -i '/WaylandEnable/d' "$config_file"
+sudo sed -i '/WaylandEnable/d' "$config_file"
 
 # Add 'WaylandEnable=false' at the end of file
-echo "WaylandEnable=false" >> "$config_file"
+sudo echo "WaylandEnable=false" >> "$config_file"
