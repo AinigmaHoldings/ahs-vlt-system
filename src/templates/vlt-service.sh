3b53da1c8dbd1c61e8b5ac755ac97c396b2bba11
nexlotlotto <81607417+nexlotlotto@users.noreply.github.com>
2024-10-15 18:22:02 -0500

    Fix for vlt.service systemct restart

--- src/templates/vlt-service.sh
+++ src/templates/vlt-service.sh
@@ -7,7 +7,10 @@ After=docker.service graphical.target
 WorkingDirectory=/home/${USER}/vlt
 User=${USER}
 EnvironmentFile=/home/${USER}/environment
-ExecStart=/usr/bin/docker-compose up -d
+ExecStart=/usr/bin/docker-compose up
+ExecStop=/usr/bin/docker-compose down
+Restart=always
+RestartSec=10
 [Install]
 WantedBy=default.target
 "
