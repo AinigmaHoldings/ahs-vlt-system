7e03593d7435ab8e417af3b6d7b314d86e83d234
nexlotlotto <81607417+nexlotlotto@users.noreply.github.com>
2024-10-29 23:45:28 -0500

    Update update-gdm3-conf.sh WaylandEnable moving to correct position

--- src/update-gdm3-conf.sh
+++ src/update-gdm3-conf.sh
@@ -1,7 +1,8 @@
 config_file="/etc/gdm3/custom.conf"
 
 # Remove all lines that contain 'WaylandEnable'
-sudo sed -i '/WaylandEnable/d' "$config_file"
+#sudo sed -i '/WaylandEnable/d' "$config_file"
 
 # Add 'WaylandEnable=false' at the end of file
-echo "WaylandEnable=false" | sudo tee -a "$config_file"
+#echo "WaylandEnable=false" | sudo tee -a "$config_file"
+sed -i 's/^#WaylandEnable=false/WaylandEnable=false/' "$CONFIG_FILE"
