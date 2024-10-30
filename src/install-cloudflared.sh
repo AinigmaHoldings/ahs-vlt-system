9bca067aaef960215c13e158a870c497c4b875fa
nexlotlotto <81607417+nexlotlotto@users.noreply.github.com>
2024-10-30 11:10:38 -0500

    Create install-cloudflared.sh

---
+++ src/install-cloudflared.sh
@@ -0,0 +1,14 @@
+echo "Uninstall cloudflared, expect some errors..."
+sudo systemctl disable cloudflared.service
+sudo apt-get remove -y --purge cloudflared
+sudo rm -rf /etc/cloudflared
+sudo systemctl disable cloudflared-update.timer
+sudo rm -rf /etc/systemd/system/cloudflared.service
+sudo rm -rf /etc/systemd/system/cloudflared-update.timer
+sudo rm -rf /etc/systemd/system/cloudflared-update.service
+sudo rm -rf cloudflared.deb 
+echo "Installing cloudflared, do not expect errors" 
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared.deb
+ 
+sudo cloudflared service install
