4c8315ebeed0409dc083875510f50bf0a636b70c
nexlotlotto <81607417+nexlotlotto@users.noreply.github.com>
2025-01-02 14:24:02 -0500

    updating to apuestasport.co

--- src/templates/config-yml.sh
+++ src/templates/config-yml.sh
@@ -1,9 +1,9 @@
 TEMPLATE_CONFIG_YML="tunnel: $cloudflaredtunnelid
 no-autoupgrade: true
 ingress:
-  - hostname: $vltName.ainigmaim.com
+  - hostname: $vltName.apuestasport.co
     service: http://localhost:9100
-  - hostname: ssh-$vltName.ainigmaim.com
+  - hostname: ssh-$vltName.apuestasport.co
     service: ssh://localhost:22
   - service: http_status:404
 "
